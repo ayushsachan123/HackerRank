4847a4307c7419190dfc981eeea3a9db2343fff7
ayushsachan123 <97084090+ayushsachan123@users.noreply.github.com>
2022-08-16 22:55:29 +0530

    Create Java_Stdin_and_StdoutI.java

---
+++ Practice Problem in Java/Java_Stdin_and_StdoutI.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        // Complete this line
+        // Complete this line
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        // Complete this line
+        // Complete this line
+    }
+}
+
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// Input--
+// 42
+// 100
+// 125
+// Output--
+// 42
+// 100
+// 125
