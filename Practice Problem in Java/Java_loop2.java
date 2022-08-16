e5f18167c719abe888a10311fa0deffd1ff74e10
ayushsachan123 <97084090+ayushsachan123@users.noreply.github.com>
2022-08-16 22:53:05 +0530

    Create Java_loop2.java

---
+++ Practice Problem in Java/Java_loop2.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// Output--
+// Hello, World.
+// Hello, Java.
