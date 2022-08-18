2493e477f0522fd2ea54e961087a36775dcc4ebd
ayushsachan123 <97084090+ayushsachan123@users.noreply.github.com>
2022-08-18 15:35:07 +0530

    Create Java_Stdin_Stdout II.java

---
+++ Practice Problem in Java/Java_Stdin_Stdout II.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        double b = s.nextDouble();
+        s.nextLine();
+        String str= s.nextLine();
+        System.out.println("String: " + str);
+         System.out.println("Double: " + b);
+          System.out.println("Int: " + a);
+    }
+}
+
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------------
+// Input--
+// 42
+// 3.1415
+// Welcome to HackerRank's Java tutorials!
+// Output--
+// String: Welcome to HackerRank's Java tutorials!
+// Double: 3.1415
+// Int: 42
+  
