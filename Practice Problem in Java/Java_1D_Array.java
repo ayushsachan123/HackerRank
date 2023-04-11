58e0287e1cfae00af37b75672d2d9cd79e85298f
ayushsachan123 <ayushsachan.in@gmail.com>
2023-04-11 19:07:28 +0530

    add Java_1D_Array.java

---
+++ Practice Problem in Java/Java_1D_Array.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Solution {
+    public static void main(String[] args) {
+       Scanner s = new Scanner(System.in);
+            int n = s.nextInt();
+            int[] a = new int[n];
+            int i;
+                for(i=0;i<n;i++){
+                    a[i]= s.nextInt();
+                }
+                for(i=0;i<n;i++){
+                    System.out.println(a[i]);
+            }
+
+    }
+}
